320fcc3ed2de45070713335e58015657d6a5d73c
Jessie Wei <jwei@seek.com.au>
2019-08-06 21:27:49 +1000

    Implement the quill editor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactQuill from 'react-quill';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CustomToolbar = () => (
+  <div id="toolbar">
+    <button className="ql-header" value="2"/>
+    <button className="ql-bold" />
+    <button className="ql-italic" />
+    <button className="ql-list" value="ordered"/>
+    <button className="ql-list" value="bullet"/>
+  </div>
+);
+
+// var icons = ReactQuill.Quill.import("ui/icons");
+// icons["header"]['2'] = "<span>Heading</span>";
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { editorHtml: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(html) {
+    this.setState({ editorHtml: html });
+  }
+
+  render() {
+    return (
+      <div className="text-editor">
+        <CustomToolbar />
+        <ReactQuill
+          onChange={this.handleChange}
+          placeholder={this.props.placeholder}
+          modules={App.modules}
+          formats={App.formats}
+          theme={"snow"} // pass false to use minimal theme
+        />
+      </div>
+    )
+  }
 }
+
+/* 
+ * Quill modules to attach to editor
+ * See https://quilljs.com/docs/modules/ for complete options
+ */
+App.modules = {
+  toolbar: {
+    container: "#toolbar",
+  },
+  clipboard: {
+    matchVisual: false,
+  }
+};
+
+/* 
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+App.formats = [
+  "header",
+  "bold",
+  "italic",
+  "list",
+  "bullet"
+];
 
 export default App;
